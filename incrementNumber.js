3ea867c7b59b4635a57de6cf01c887c347b11b26
Artem Mozhegov <amozhegov@yahoo.com>
2021-09-10 01:12:31 +0300

    Initial commit

---
+++ incrementNumber.js
@@ -0,0 +1,7 @@
+function incrementButton() {
+	var element = document.getElementById("incrementText");
+	var value = element.innerHTML;
+	++value;
+	console.log(value)
+	document.getElementById('incrementText').innerHTML = value;
+}
